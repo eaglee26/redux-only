fb5bf8a33c80205c0be737cce9ac8d6051a57474
Sergio <sergiiy@tallium.com>
2019-12-30 23:25:14 +0200

    redux-ui and random button

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ const reducer = (state = 0, action) => {
     switch (action.type) {
         case 'INC':
             return state + 1;
+
+        case 'DEC':
+            return state - 1;
+
+        case 'RND':
+            return state + action.payload;
+
         default:
             return state;
     }
@@ -13,12 +20,27 @@ const reducer = (state = 0, action) => {
 
 const store = createStore(reducer);
 
-store.subscribe(() => {
-    console.log(store.getState())
+document.getElementById('inc').addEventListener('click', () => {
+    store.dispatch({ type: 'INC'});
 });
 
-store.dispatch({ type: 'INC' });
-store.dispatch({ type: 'INC' });
+document.getElementById('dec').addEventListener('click', () => {
+    store.dispatch({ type: 'DEC'});
+});
+
+document.getElementById('rnd').addEventListener('click', () => {
+    const payload = Math.floor(Math.random() * 10);
+    store.dispatch({
+        type: 'RND',
+        payload
+    });
+});
+
+const update = () => {
+    document.getElementById('counter').innerHTML = store.getState();
+};
+
+store.subscribe(update);
 
 
 
